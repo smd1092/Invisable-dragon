9935bc5466adbd97b529476d5d159f67701abae9
Park ChaeWon <smd1092@naver.com>
2019-11-08 15:45:42 +0900

    Signed-off-by: Park ChaeWon <smd1092@naver.com>

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void renderScene()
 	glLoadIdentity();
 	gluLookAt(0.0f, 10.0f, 10.0f, 0.0f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f);
 
-	glClearColor(1.0f,1.0f,1.0f,1.0f);
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	glEnable(GL_DEPTH_TEST);
